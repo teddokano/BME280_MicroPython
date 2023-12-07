fc224100ca6f90354f96aca6e6afb57a7173e5f6
Tedd OKANO <okano.akifumi@gmail.com>
2023-12-08 05:42:54 +0900

    changed to adapt new bbI2C constructor

--- BME280.py
+++ BME280.py
@@ -351,9 +351,9 @@ def BME280( interface, address = DEFAILT_ADDRESS, cs = None ):
 
 def main():
 	#intf	= I2C( 0, sda = Pin( 0 ), scl = Pin( 1 ) )
-	#intf	= bbI2C( 0, 1 )
+	intf	= bbI2C( sda = Pin( 0 ), scl = Pin( 1 ) )
 	#intf	= SPI( 1, 1000000, sck = Pin( 10 ), mosi = Pin( 11 ), miso = Pin( 12 ) )
-	intf	= bbSPI( sck = Pin( 10 ), mosi = Pin( 11 ), miso = Pin( 12 ), cs = Pin( 13 ) )
+	#intf	= bbSPI( sck = Pin( 10 ), mosi = Pin( 11 ), miso = Pin( 12 ), cs = Pin( 13 ) )
 	
 	bme		= BME280( intf )	
 	bme.show_dump()
